b2e7895c8931d33f04cc9632e1e556de85653888
joslin1215 <55719863@qq.com>
2024-01-09 14:34:53 +0800

    feat: 新增coze机器人页面布局调整

---
+++ coze-layout.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         coze-layout
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-09.01
+// @description  coze.com机器人页面布局控制
+// @author       JosLin1215
+// @match        https://www.coze.com/space/*/bot/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=coze.com
+// @grant        none
+// @license      GPL-2.0-only
+// @downloadURL https://raw.githubusercontent.com/joslin1215/tampermonkey-script/main/coze-layout.js
+// @updateURL https://raw.githubusercontent.com/joslin1215/tampermonkey-script/main/coze-layout.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let _changeCnt = 0;
+    const _targetSelector = "#root > div > div > div > div > div.semi-space.semi-space-align-center.semi-space-horizontal";
+
+    function changeLayout(){
+        console.log('-------------------------------------------');
+        let div = document.querySelector("#root > div > div > div > div > div.sidesheet-container");
+        let gridTemplateColumns = ++_changeCnt%2 == 0 ? '13fr 13fr 14fr' : '0fr 0fr 14fr'
+        div.style.gridTemplateColumns = gridTemplateColumns;
+    }
+
+    function addBtn(){
+        console.log("addBtn");
+        if(_changeCnt > 0) return;
+        console.log('============================================');
+
+        // 将按钮元素添加到页面中的某个容器元素中
+        var containerElement = document.querySelector(_targetSelector); // 替换为实际容器的 ID
+        if(!containerElement) return;
+        clearInterval(intervalId);
+
+        // 创建按钮元素
+        var buttonElement = document.createElement('button');
+
+        // 设置按钮元素的属性
+        buttonElement.className = 'semi-button';
+        buttonElement.type = 'button';
+        buttonElement.setAttribute('aria-disabled', 'false');
+        buttonElement.onclick = changeLayout;
+
+        // 创建 span 元素
+        var spanElement = document.createElement('span');
+
+        // 设置 span 元素的类名和属性
+        spanElement.className = 'semi-button-content';
+        spanElement.setAttribute('x-semi-prop', 'children');
+
+        // 设置 span 元素的文本内容
+        spanElement.textContent = 'Layout';
+
+        // 将 span 元素作为按钮元素的子元素添加
+        buttonElement.appendChild(spanElement);
+
+        containerElement.appendChild(buttonElement);
+    }
+
+    var intervalId = setInterval(addBtn, 100);
+
+})();
